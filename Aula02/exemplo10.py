a147383d1f9d20eddce15e25f1a8e483bfad489f
Edwillie <edwillie@gmail.com>
2025-10-15 20:32:27 -0300

    antes intervalo

---
+++ Aula02/exemplo10.py
@@ -0,0 +1,4 @@
+numero = int(input("Entre com um numero: "))
+
+if (numero > 10) and (numero < 20):
+    print("O número está entre 10 e 20")
---
+++ Aula02/exemplo11.py
@@ -0,0 +1,7 @@
+idade = 18
+cliente_fiel = True
+
+if (idade < 18) or cliente_fiel:
+    print("Desconto aplicável!")
+else:
+    print("Sem desconto!")
