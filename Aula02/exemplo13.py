1ad725213b977a1ac1d03113a18b21f583e53ef7
Edwillie <edwillie@gmail.com>
2025-10-15 21:46:54 -0300

    partindo pro fechamento da aula

---
+++ Aula02/exemplo13.py
@@ -0,0 +1,9 @@
+a = int(input("conte até: "))
+i = 0
+
+if (a > 0):
+    while i<=a:
+        print(i)
+        i += 1
+else:
+    print("Entre com um numero positivo.")
---
+++ Aula02/exemplo14.py
@@ -0,0 +1,7 @@
+print("======= TABUADA =======")
+tab = int(input("tabuada de qual número? "))
+
+i = 1
+while i <= 10:
+    print(f"{tab} x {i} = {tab * i}")
+    i += 1
