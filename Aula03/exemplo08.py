32ed39e5219c13e77972359b7ef7b38df10a985c
Edwillie <edwillie@gmail.com>
2025-10-20 21:16:03 -0300

    intervalo

---
+++ Aula03/exemplo08.py
@@ -0,0 +1,4 @@
+minha_lista = [8, 0, 4, 3, 5, 1, 2]
+tamnhoLista = len(minha_lista)
+
+print("O tamanho da lista é, ", tamnhoLista)
---
+++ Aula03/exemplo09.py
@@ -0,0 +1,14 @@
+minha_lista = [5, 15, 8, 9, 10, 12]
+
+for i in range(len(minha_lista)):
+    print("Elemento", i+1, "da lista é: ", minha_lista[i])
+
+print("="*20)
+for numero in minha_lista:
+    print(numero)
+
+print("="*20)
+i=0
+while i < (len(minha_lista)):
+    print(minha_lista[i])
+    i += 1
