d26ef2bd75bef49467b4ac0b26b4f5fd8ad98988
Edwillie <edwillie@gmail.com>
2025-10-22 21:59:20 -0300

    fechando aula

--- Aula04/2-Numpy/exemplo11.py
+++ Aula04/2-Numpy/exemplo11.py
@@ -2,3 +2,9 @@ import numpy as np
 
 aleat = np.random.rand(5)
 print("Aleatoria \n", aleat)
+
+inteiros = np.random.randint(1,100,5)
+print("Aleatorio Inteiro \n", inteiros)
+
+normal = np.random.randn(5)
+print("Distribuicao Normal: \n", normal)
