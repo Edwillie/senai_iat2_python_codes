2be36eb57d5402e82e776396417917f158124af5
Edwillie <edwillie@gmail.com>
2025-12-03 19:48:48 -0300

    links

--- Aula10/app.py
+++ Aula10/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
-@app.route("/")
 
+@app.route("/")
 def welcome():
-    return "Bem vindo ao site em flask!"
+    return render_template("index.html")
+
+@app.route("/contatos")
+def contato():
+    return render_template("contatos.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
